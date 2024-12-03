2f914c3e0db316493b8e781ce3e5ec7c5cdedf6b
danel <deguesju23dw@ikzubirimanteo.com>
2024-12-03 23:17:35 +0100

    arreglos en inventario

--- src/Pestañas/Inventario/index.jsx
+++ src/Pestañas/Inventario/index.jsx
@@ -12,7 +12,7 @@ const Inventory = () => {
       name: "AK-47 | Asiimov",
       wear: "Factory New",
       price: 150.50,
-      image: "",
+      image: "/img/m4a4.png",
       status: "available"
     },
     {
@@ -20,7 +20,7 @@ const Inventory = () => {
       name: "M4A4 | Neo-Noir",
       wear: "Minimal Wear",
       price: 89.99,
-      image: "",
+      image: "/img/m4a4.png",
       status: "available"
     },
     {
@@ -28,11 +28,35 @@ const Inventory = () => {
       name: "AWP | Dragon Lore",
       wear: "Field-Tested",
       price: 1500.00,
-      image: "",
+      image: "/img/deagle.png",
       status: "on_sale"
+    },
+    {
+      id: 4,
+      name: "Desert Eagle | Blaze",
+      wear: "Factory New",
+      price: 420.69,
+      image: "/img/deagle.png",
+      status: "available"
+    },
+    {
+      id: 5,
+      name: "Karambit | Fade",
+      wear: "Factory New",
+      price: 1200.00,
+      image: "/img/glock.png",
+      status: "locked"
+    },
+    {
+      id: 6,
+      name: "Glock-18 | Fade",
+      wear: "Minimal Wear",
+      price: 890.00,
+      image: "/img/glock.png",
+      status: "available"
     }
   ];
-  
+
   const {
     sortBy,
     setSortBy,
@@ -54,13 +78,15 @@ const Inventory = () => {
   } = useInventoryActions();
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <InventoryHeader 
-        totalItems={totalItems}
-        onSort={(e) => setSortBy(e.target.value)}
-        onFilter={() => {/* lógica de filtros */}}
-      />
-      <InventoryGrid items={sortedAndFilteredItems} />
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow w-full max-w-[1600px] mx-auto px-4 sm:px-6 lg:px-12 pt-24 pb-8">
+        <InventoryHeader 
+          totalItems={totalItems}
+          onSort={(e) => setSortBy(e.target.value)}
+          onFilter={() => {/* lógica de filtros */}}
+        />
+        <InventoryGrid items={sortedAndFilteredItems} />
+      </div>
     </div>
   );
 };
